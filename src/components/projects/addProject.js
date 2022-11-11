24ddeb37b99c3dd7102d2c746ba1153c9127aed2
CryptoDevWill <williamdsanders05@gmail.com>
2022-11-11 03:07:15 -0500

    Spacing

--- src/components/projects/addProject.js
+++ src/components/projects/addProject.js
@@ -7,12 +7,13 @@ export default function AddProject(){
     const [github, setGithub ] = useState('')
     const [ description, setDescription ] = useState('')
     const [ budget, setBudget ] = useState(0)
-    const [ category, setCategory ] = useState('')
-    const [ type, setType ] = useState('')
+    const [ category, setCategory ] = useState('blog')
+    const [ type, setType ] = useState('website')
     let { addProject } = useProject();
 
     //Add project to database
     const handleAddProject = ()=>{
+        console.log(category)
         console.log(type)
             addProject({
                 title,
@@ -30,14 +31,20 @@ export default function AddProject(){
     return(
         <div style={{display: 'flex', flexDirection: 'column', width: 400, margin: 10, padding: 5, border: '1px solid black'}}>
             <h3>Add Project</h3>
-            <input value={title} placeholder='Title' onChange={(e)=> setTitle(e.target.value)} />
-            <textarea value={description} onChange={(e)=> setDescription(e.target.value)} name="Text1" cols="40" rows="5"></textarea>
-            <input value={github} placeholder='Github link https://projectlink.git' onChange={(e)=> setGithub(e.target.value)} />
-            <input value={category} placeholder='Category' onChange={(e)=> setCategory(e.target.value)} />
-            <input value={type} placeholder='Type' onChange={(e)=> setType(e.target.value)} />
-            <label htmlFor="projectBudget">Project Budget:</label>
-            <input id="projectBudget" type="number" name="projectBudget" value={budget} onChange={(e)=> setBudget(e.target.value)} />   
-            <button onClick={handleAddProject}>Add Project</button>
+            <input style={{margin: 5, padding: 5}} value={title} placeholder='Title' onChange={(e)=> setTitle(e.target.value)} />
+            <textarea style={{margin: 5, padding: 5}} value={description} onChange={(e)=> setDescription(e.target.value)} placeholder="Enter project description" cols="40" rows="5"></textarea>
+            <input style={{margin: 5, padding: 5}} value={github} placeholder='Github link https://projectlink.git' onChange={(e)=> setGithub(e.target.value)} />
+            <select style={{margin: 5, padding: 5}} value={category} onChange={(e)=> setCategory(e.target.value)}>
+                <option value="blog">Blog</option>
+                <option value="ecommerce">Ecommerce</option>
+            </select>
+            <select style={{margin: 5, padding: 5}} value={type} onChange={(e)=> setType(e.target.value)}>
+                <option value="website">Website</option>
+                <option value="api">Api</option>
+            </select>
+            <label style={{margin: 5, padding: 5}} htmlFor="projectBudget">Project Budget:</label>
+            <input style={{margin: 5, padding: 5}} id="projectBudget" type="number" name="projectBudget" value={budget} onChange={(e)=> setBudget(e.target.value)} />   
+            <button style={{margin: 5, padding: 5}} onClick={handleAddProject}>Add Project</button>
         </div>
     )
 }
