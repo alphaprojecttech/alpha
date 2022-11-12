bda81844768d7db582e58d25929574ca81457c6d
hr-sobuj <sobujhd@gmail.com>
2022-11-12 19:26:38 +0600

    form bug fixed

--- src/components/Task/AddTask.js
+++ src/components/Task/AddTask.js
@@ -1,7 +1,10 @@
 export default function AddTask() {
     return (
         <>
-            <form className="w-1/2 border">
+            <form className="w-1/2 border flex flex-col space-y-3">
+                <div className="mb-5">
+                    <h1 className="text-4xl font-bold">Task Create Form</h1>
+                </div>
                 <div>
                     <h1>Task Name</h1>
                     <input type="text" className="border" name="" placeholder="Task Name" />       
@@ -24,7 +27,7 @@ export default function AddTask() {
                 </div>
                 <div className="border">
                     <h1>Amount</h1>
-                    <input type="number" className="border" name="" placeholder="Task Name" />
+                    <input type="number" className="border" name="" placeholder="Amount" />
                 </div>
                 <div className="border">
                     <h1>Upload Image</h1>
@@ -35,6 +38,9 @@ export default function AddTask() {
                 </div>
                 <div className="border">
                     <input type="checkbox" name="" id="" /> Complete
+                </div>
+                <div className="border">
+                    <button className="bg-blue-200 px-2 py-1">Create Task</button>
                 </div>
             </form>
         </>
